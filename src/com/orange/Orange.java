5e632fef074d96278ffafb3c322b7451e3d1ec4e
Nivethini L <crazy7nini@gmail.com>
2023-08-29 16:00:26 +0530

    Configure Browser

---
+++ src/com/orange/Orange.java
@@ -0,0 +1,23 @@
+package com.orange;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Orange {
+	public static void main(String[] args) {
+		
+		//configure browser and web page
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.orangehrm.com/");
+		driver.manage().window().maximize();
+		
+		//title of the page
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		//URL
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+	}
+
+}
